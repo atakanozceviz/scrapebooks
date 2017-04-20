d82af2bce638e567ee3907e291961daed5531a6e
atakanozceviz <atakanozceviz@hotmail.com>
2017-04-20 15:27:23 +0300

    Minor fix for JSON

--- main.go
+++ main.go
@@ -69,7 +69,7 @@ func jsonp(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type:", "application/json;charset=utf-8")
 		w.Write([]byte(jp))
 	} else {
-		jp := cb + `("err": "FormEmpty")`
+		jp := cb + `({"err": "FormEmpty"})`
 		w.Write([]byte(jp))
 	}
 }
