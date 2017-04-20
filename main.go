5eb183851e85cc894139a9bf29308c3087a8dd78
atakanozceviz <atakanozceviz@hotmail.com>
2017-04-20 15:24:16 +0300

    Changes for jsonp

--- main.go
+++ main.go
@@ -69,6 +69,7 @@ func jsonp(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type:", "application/json;charset=utf-8")
 		w.Write([]byte(jp))
 	} else {
-		w.Write([]byte("Wrong request!"))
+		jp := cb + `("err": "FormEmpty")`
+		w.Write([]byte(jp))
 	}
 }
